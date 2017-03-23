a6b1db05a4eb3a5a7e847fa2c17ee12227fd8d80
diegous <munoa.d@gmail.com>
2017-03-23 12:32:43 -0300

    First commit

---
+++ js/fileLoader.js
@@ -0,0 +1,32 @@
+const fs = require('fs')
+const knex = require('knex')({
+  client: 'sqlite3',
+  connection: {
+    filename: "./mydb.sqlite"
+  },
+  useNullAsDefault: true
+})
+
+knex.schema.createTableIfNotExists('files', function (table) {
+  table.increments()
+  table.string('path')
+  table.text('data')
+  table.timestamps()
+})
+
+const loadFile = (path) => {
+  fs.readFile(path, 'utf-8', (err, data) => {
+    knex('files')
+      .insert({path: path, data: data})
+      .then(console.log)
+  })
+}
+
+const loadData = (el) => {
+  knex
+    .select('path', 'data')
+    .from('files')
+    .then((data) => {
+      el.textContent = data
+    })
+}
