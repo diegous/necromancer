ac69ae832edd1076f525bd293c03430257d1cb23
diegous <munoa.d@gmail.com>
2017-04-23 04:20:46 -0300

    Changed button to load entire folder instead of single file. Adjusted regular expressions to accept all files in sample

--- js/fileMatchers.js
+++ js/fileMatchers.js
@@ -4,22 +4,22 @@ const headerSubtitleRegex =           /│           Dr. Enrique C. Muñoa      
 const headerAddressRegex =            /│   Roca 279∙\(8324\) Cipolletti∙Río Negro  │                          │/
 const headerProtocolRegex =           /├───────── Teléfono: \(0943\) 74373 ────────┘\s*Protocolo N°\s*(\d+)\s*│/
 const headerBlankRegex =              /│                                                                    │/
-const headerPatientDataRegex =        /│Paciente: (.+) Edad: (\d+)\s*│/
+const headerPatientDataRegex =        /│Paciente:(.+)Edad:(.+)\s*│/
 //headerBlankRegex                    /│                                                                    │/
-const headerDoctorAndInsuranceRegex = /│Solicitado por (.+)\s+O. Social: (.+) │/
+const headerDoctorAndInsuranceRegex = /│Solicitado por (.+)O. Social:(.+)│/
 const headerBottomBorderRegex =       /└────────────────────────────────────────────────────────────────────┘/
-const materialRegex =                 /Material (?:R|r)emitido:(.+)/
+const materialRegex =                 /Material (?:R|r)emitido\s*:(.+)/
 //blank                               //
 const reportTitleRegex =              /\s*(INFORME .+)/
 const reportTitleUnderscoreRegex =    /\s+=+/
 //blank                               //
-const descriptionRegex =              /(DESCRIPCION \S.*:\s*\S?.*)/
+const descriptionRegex =              /(DESCRIPCI(?:o|ó)N \S.*:\s*\S?.*)/gi
 const paragraphRegex =                /\s*(\S.*)/
 //blank                               //
-const diagnosisRegex =                /(DIAGNOSTICO \S.*:\s*\S?.*)/
+const diagnosisRegex =                /(DIAGN(?:o|ó)STICO \S.*:\s*\S?.*)/gi
 //blank                               //
 //blank                               //
 //blank                               //
-const signatureRegex =                /\s*Dr. Enrique C. Muñoa/
+const signatureRegex =                /\s*Dr. Enrique.+Muñoa/
 const signature2Regex =               /\s*Médico Anatomopatólogo/
-const dateRegex =                     /\s*Cipolletti, (.+).\s*M.R.N. 1045  M.Nqn. 1098/
+const dateRegex =                     /\s*Cipolletti,(.+).\s*M.R.N. 1045  M.Nqn. 1098/
